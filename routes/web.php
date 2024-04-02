1f2a49f5d4a522d9439e4fecc73f0ed4bcb7368a
jOvsNeverio <nevsjovi@gmail.com>
2024-04-02 17:56:47 +0800

    My first project in laravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('posts.index');
+});
+
+Route::get('/post/{post}', function ($post) {
+    return view('posts.show', [
+    'post' => $post
+    ]);
+
+});
